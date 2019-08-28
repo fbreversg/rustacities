7e88cc8886ec84115c21e7072e62631a2e4a88e1
fbreversg <etiorum@gmail.com>
2019-08-28 12:51:53 +0200

    Example of invalid copy.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -5,3 +5,11 @@ fn main() {
 
 	println!("{}", s); // This will print `hello, world!`
 }
+
+fn invalid_copy() {
+	let s1 = String::from("hello");
+	let s2 = s1;	//From now s1 is invalidated and only s2 has the data
+
+	println!("{}, world!", s1);
+
+}
