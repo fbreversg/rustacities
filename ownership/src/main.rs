5f55a02451595210c4d3b1ae544609a62f7da49e
fbreversg <etiorum@gmail.com>
2019-08-28 12:54:36 +0200

    Clone and copy (fixed sized).

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -13,3 +13,19 @@ fn invalid_copy() {
 	println!("{}, world!", s1);
 
 }
+
+fn valid_copy() {
+	let x = 5;
+	let y = x;		//Valid copy since its fixed size and being on stack, not on heap.
+
+	println!("x = {}, y = {}", x, y);
+}
+
+fn clone_copy() {
+	let s1 = String::from("hello");
+	let s2 = s1.clone(); 	//hard copy. Copies stack and heap!
+
+	println!("s1 = {}, s2 = {}", s1, s2);
+
+}
+
